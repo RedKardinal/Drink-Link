97a54fe20b3f17f7179b7e4896609acb66a02f24
John Lestock <lestock17@gmail.com>
2019-10-20 14:23:30 -0500

    Working map in class component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,28 +3,55 @@ import { connect } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import MapChild from '../MapChild/MapChild';
 import { GoogleMap } from 'react-google-maps';
+import './Map.css'
 
 
 class Map extends Component {
 
     componentDidMount() {
         this.getLocations();
+        this.renderMap();
     } // end componentDidMount
 
     getLocations = () => {
         this.props.dispatch({ type: 'FETCH_LOCATION' })
     }
 
+    renderMap = () => {
+        loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&callback=initMap`)
+        window.initMap = this.initMap
+    }
+
+    initMap = () => {
+
+        // Create A Map
+        var map = new window.google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        })
+    }
+
 
     render () {
         return (
             <Router>
-                <div>
-                    <MapChild/>
+                <div id="map">
+                    Test text
+                    {/* <MapChild/> */}
+
                 </div>
            </Router>
         )
     }
+}
+
+function loadScript(url) {
+    var index  = window.document.getElementsByTagName("script")[0]
+    var script = window.document.createElement("script")
+    script.src = url
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
 }
 
 const mapStateToProps = (reduxStore) => ({
